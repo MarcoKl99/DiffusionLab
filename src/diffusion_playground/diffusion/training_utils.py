fix: Correct time tensor dimensionality to match for real image data, correct alpha bar calculation in noising process, move experiment results to code (notebooks)

src/diffusion_playground/diffusion/training_utils.py
@@ -2,20 +2,28 @@ import torch
 from .noise_schedule import LinearNoiseSchedule
 
 
-def sample_xt(x0: torch.Tensor, noise_schedule: LinearNoiseSchedule) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+def sample_xt(
+        x0: torch.Tensor,
+        noise_schedule: LinearNoiseSchedule,
+        t: torch.Tensor | None = None
+) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
     batch_size = x0.shape[0]
     device = x0.device
 
-    # Get a random time step
-    t = torch.randint(
-        low=0,
-        high=noise_schedule.time_steps,
-        size=(batch_size,),
-        device=device
-    )
+    # Get a random time step if no time step was given
+    if t is None:
+        t = torch.randint(
+            low=0,
+            high=noise_schedule.time_steps,
+            size=(batch_size,),
+            device=device
+        )
 
     # Get the corresponding alpha bar
-    alpha_bar_t = noise_schedule.alphas[t].unsqueeze(1)
+    alpha_bar_t = noise_schedule.alpha_bars[t].unsqueeze(1)
+
+    # Dynamic reshape for correct broadcasting
+    alpha_bar_t = alpha_bar_t.view(batch_size, *([1] * (x0.dim() - 1)))
 
     # Create the random noise
     noise = torch.randn_like(x0)
@@ -23,4 +31,4 @@ def sample_xt(x0: torch.Tensor, noise_schedule: LinearNoiseSchedule) -> tuple[to
     # Add the noise to x0
     xt = torch.sqrt(alpha_bar_t) * x0 + torch.sqrt(1 - alpha_bar_t) * noise
 
-    return xt, noise, t.float().unsqueeze(1)
+    return xt, noise, t

src/diffusion_playground/models/mlp_denoiser.py
@@ -15,8 +15,10 @@ class MLPDenoiser(nn.Module):
         )
 
     def forward(self, x: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
-        # Add the timestep to the input date
+        if t.dim() == 1:
+            t = t.unsqueeze(1)
+
+        t = t.float()
         x_0 = torch.cat([x, t], dim=1)
 
-        # Pass the data including timestep through the network
         return self.net(x_0)